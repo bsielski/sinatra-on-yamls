d49ad82f8997b0f2eb0b9103c5ff6510a546713b
Bartłomiej Sielski <b.sielski@yandex.com>
2018-10-06 18:07:53 +0200

    Delete useless things

--- app.rb
+++ app.rb
@@ -1,10 +1,5 @@
 require_relative "./cells/home_page/home_page_cell"
 
-require_relative "./models/record"
-require_relative "./cells/task/task_index_cell"
-require_relative "./cells/task/task_new_cell"
-require_relative "./cells/task/task_delete_cell"
-
 require_relative "./models/note"
 require_relative "./cells/note/note_index_cell"
 require_relative "./cells/note/note_new_cell"
@@ -12,41 +7,11 @@ require_relative "./cells/note/note_edit_cell"
 require_relative "./cells/note/note_delete_cell"
 
 require_relative "./cells/layout/html_layout_cell"
-#require 'securerandom'
+
 require 'sinatra'
 
 get '/' do
   HomePageCell.new(nil, layout: HtmlLayoutCell).call
-end
-
-get '/tasks' do
-  model = Record::All.new("tasks").call
-  TaskIndexCell.new(model, layout: HtmlLayoutCell).call
-end
-
-get '/tasks/new' do
-  model = {data: ""}
-  TaskNewCell.new(model, layout: HtmlLayoutCell).call
-end
-
-post '/tasks' do
-  Record::Save.new("tasks").call(SecureRandom.uuid, params[:task][:data])
-  redirect to('/tasks'), 303
-end
-
-get '/tasks/delete/:id' do
-  model = Record::Load.new("tasks").call(params["id"])
-  TaskDeleteCell.new(model, layout: HtmlLayoutCell).call
-end
-
-post '/tasks/delete/:id' do
-  Record::Delete.new("tasks").call(params["id"])
-  redirect to('/tasks'), 303
-end
-
-delete '/tasks/:id' do
-  Record::Delete.new("tasks").call(params["id"])
-  redirect to('/tasks'), 303
 end
 
 # NOTES
--- cells/menu_bar/cell/menu_bar.rb
+++
@@ -1,7 +0,0 @@
-require "cell"
-require "cell/erb"
-
-class MenuBarCell < Cell::ViewModel
-  self.view_paths = ["views"]
-  include ::Cell::Erb
-end
